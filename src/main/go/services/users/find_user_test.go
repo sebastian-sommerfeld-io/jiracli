1be79ea37531d199cc63f13813fe1aa7b6581b5b
sebastian <sebastian@sommerfeld.io>
2023-02-14 16:42:36 +0000

    minor update

--- src/main/go/services/users/find_user_test.go
+++ src/main/go/services/users/find_user_test.go
@@ -1,8 +1,9 @@
 package users
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func userFoundAssertions(t *testing.T, user User, err error) {
