f720a709d621f2b9f561a92fe65f5b55930af933
sebastian <sebastian@sommerfeld.io>
2023-02-15 19:43:51 +0000

    demo calls to jira api

---
+++ src/main/postman/demo-api-calls.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# @file demo-api-calls.sh
+# @brief Invoke some demo calls to the Jira API to learn about the Rest API.
+#
+# @description This script invokes some demo calls to the Jira API to learn about the Rest API.
+#
+# === Script Arguments
+#
+# The script does not accept any parameters.
+#
+# === Script Example
+#
+# [source, bash]
+# ```
+# ./demo-api-calls.sh
+# ```
+
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+
+# Include local bash modules
+source "../../util/bash-modules/log.sh"
+
+
+# @description Wrapper function to not repeat myself when calling the Jira API lots of times.
+#
+# @example
+#    callEndpoint /rest/plugins/applications/1.0/installed/jira-software/license
+#
+# @arg $@ String The API endpoint - mandatory
+#
+# @exitcode 8 If param API endpoint string is missing
+function callEndpoint() {
+  if [ -z "$1" ]; then
+    LOG_ERROR "API endpoint missing"
+    LOG_ERROR "exit" && exit 8
+  fi
+
+  readonly USER="admin"
+  readonly PASS="admin"
+  readonly BASE_URL="http://localhost:8080"
+
+  LOG_HEADER "Call API endpoint $1"
+  LOG_INFO "User = $USER"
+
+  curl -u "$USER:$PASS" -H "Content-Type: application/json" "${BASE_URL}${1}" # | json_pp
+}
+
+callEndpoint /rest/plugins/applications/1.0/installed/jira-software/license
