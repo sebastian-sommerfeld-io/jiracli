429043f04450282323f3b20d24e8b854500683e0
sebastian <sebastian@sommerfeld.io>
2023-03-07 21:33:20 +0000

    changed var name

--- src/util/workflows/cd/validate-version/index.mjs
+++ src/util/workflows/cd/validate-version/index.mjs
@@ -1,7 +1,7 @@
 import assert from 'node:assert';
 import { createRegExp, exactly, digit, oneOrMore } from 'magic-regexp';
 
-const VERSION_DEFINITION = createRegExp(
+const VERSION_REGEX = createRegExp(
   exactly('v')
     .and(oneOrMore(digit).groupedAs('major'))
     .and(exactly('.').and(oneOrMore(digit).groupedAs('minor')))
@@ -11,14 +11,14 @@ const VERSION_DEFINITION = createRegExp(
 console.log('----- valid version syntax ---------------');
 console.log('v0.1.0');
 console.log('v0.1.0-SNAPSHOT');
-console.log(VERSION_DEFINITION);
+console.log(VERSION_REGEX);
 console.log('------------------------------------------');
 
 // samples for valid versions
-assert.equal(VERSION_DEFINITION.test(true, 'v0.1.0'));
-assert.equal(VERSION_DEFINITION.test(true, 'v0.1.0-SNAPSHOT'));
+assert.equal(VERSION_REGEX.test(true, 'v0.1.0'));
+assert.equal(VERSION_REGEX.test(true, 'v0.1.0-SNAPSHOT'));
 
 // samples for invalid versions
-assert.equal(VERSION_DEFINITION.test(false, '0.1'));
-assert.equal(VERSION_DEFINITION.test(false, '0.1.0'));
-assert.equal(VERSION_DEFINITION.test(false, '0-1-0'));
+assert.equal(VERSION_REGEX.test(false, '0.1'));
+assert.equal(VERSION_REGEX.test(false, '0.1.0'));
+assert.equal(VERSION_REGEX.test(false, '0-1-0'));
